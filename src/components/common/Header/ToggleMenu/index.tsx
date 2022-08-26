2da697f5c7f780a9bf26849f538198c8a43b7019
Yura Chekhov <ychekhov@bk.ru>
2022-08-27 02:41:16 +0300

    refactor media mainPage

--- src/components/common/Header/ToggleMenu/index.tsx
+++ src/components/common/Header/ToggleMenu/index.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react';
-import { motion } from 'framer-motion';
 import ToggleNav from './ToggleNav';
 import style from './ToggleMenu.module.scss';
 
